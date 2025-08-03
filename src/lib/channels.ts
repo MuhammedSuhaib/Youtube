b5308134b9e84be519ef2969c705edb73066e069
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-08-03 18:14:18 +0500

    ok

---
+++ src/lib/channels.ts
@@ -0,0 +1,17 @@
+export const sources = [
+  //Single videos
+  { type: "video", id: "uI2SbqgU_ng" },
+  // // Playlists
+  { type: "playlist", id: "PLbVdwtmx18svoi3cm--oZj6Y91sGjNN1W" },
+  { type: "playlist", id: "PLdZNFVCDo_1cjL63hiyBujpQrQ0j0W-Pq" },
+  { type: "playlist", id: "PLkNAZQemmH01lOguWGxvmbMOfEPDcr_LT" },
+
+  // Channels
+  { type: "channel", id: "UCTFkdBuVfan9Y564dRx2eZg" },
+  { type: "channel", id: "UCJ3ACnpsZX-AeZGuKSRulpQ" },
+  { type: "channel", id: "UC4JCksJF76g_MdzPVBJoC3Q" },
+  { type: "channel", id: "UCe9JSDmyqNgA_l2BzGHq1Ug" },
+  { type: "channel", id: "UCCBGUffdWwRV0gUqgElkCfw" },
+  { type: "channel", id: "UCF2ltLX1dnPa6-L1paPaMzw" },
+];
+        
